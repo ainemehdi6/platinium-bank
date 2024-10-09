b7af0817c12316279b8f8e0eff20bb16429155d8
Arthur <arthur.wlodarczak@laposte.net>
2024-10-09 11:32:15 +0200

    feat : deposit, transac and logout

--- src/dab/dab.controller.ts
+++ src/dab/dab.controller.ts
@@ -1,15 +1,35 @@
 import { Controller, Post, Get, Body, Param, Patch, UseGuards } from '@nestjs/common';
 import { DabService } from './dab.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 
 @Controller('dab')
 export class DabController {
-    constructor(private readonly dabService: DabService) {}4
+    constructor(private readonly dabService: DabService) {}
 
     @Post('login')
     async login(@Body() loginDto: { cardNumber: string; pin: string }) {
       return this.dabService.login(loginDto.cardNumber, loginDto.pin);
     }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('transfer')
+    async transfer(@Body() transferDto: { fromAccountId: number; toAccountId: number; amount: number }) {
+        return this.dabService.transfer(transferDto.fromAccountId, transferDto.toAccountId, transferDto.amount);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('deposit')
+    async depositCheck(@Body() depositDto: { accountId: number; amount: number; delay?: number }) {
+        return this.dabService.depositCheck(depositDto.accountId, depositDto.amount, depositDto.delay);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Patch('logout')
+    async logout() {
+        return this.dabService.logout();
+    }
+
 
     @Get('accounts/balance')
     async getAccountsBalance() {
